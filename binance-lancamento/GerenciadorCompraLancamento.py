f99519e93b0346841863046dd448020392835a08
zarpsystem_asantos <zarpsystem_asantos@orizon.com.br>
2024-02-28 23:21:07 -0300

    Alteracao no gerenciador de moedas

--- binance-lancamento/GerenciadorCompraLancamento.py
+++ binance-lancamento/GerenciadorCompraLancamento.py
@@ -4,7 +4,7 @@ import json
 
 from BinanceCompraLancamento import BinanceCompraLancamento
 from BinanceSpotBalance import BinanceSpotBalance, check_balance_min
-from GerenciadorMoedas import GerenciadorMoedas
+from GerenciadorMoedasLancamento import GerenciadorMoedasLancamento
 from decouple import config
 
 class GerenciadorCompraLancamento:
@@ -24,7 +24,7 @@ class GerenciadorCompraLancamento:
         # Configurar o logger com base nas configurações do JSON
         logging.basicConfig(filename=config_data['filenamelog'], level=getattr(logging, config_data['level_log']), format='%(asctime)s - %(message)s', datefmt='%d-%m-%Y %H:%M:%S')
 
-        gerenciador = GerenciadorMoedas(DATABASE_ADDRESS)
+        gerenciador = GerenciadorMoedasLancamento(DATABASE_ADDRESS)
 
         compradorBinance = BinanceCompraLancamento(api_key, api_secret, DATABASE_ADDRESS, self.config_file_path)
         binance_balance = BinanceSpotBalance(api_key, api_secret)
@@ -36,12 +36,12 @@ class GerenciadorCompraLancamento:
                 if moedas:
                     for nome_moeda in moedas:
                         #logging.info(f'O MFI para {nome_moeda} -> : {round(mfi_value, 2)}')
-                        logging.info(f'A criptomoeda {nome_moeda[0]} está no processo de compra.\n')
+                        logging.info(f'A criptomoeda {nome_moeda} está no processo de compra.\n')
                         account_balances = binance_balance.get_balance()
                         balance = check_balance_min(account_balances, symbol_to_check, minimum_balance)
 
                         if balance >= minimum_balance:
-                            compradorBinance.comprar(nome_moeda[0])
+                            compradorBinance.comprar(nome_moeda)
                             # logging.info(f"O saldo é  {balance}.\n")
                         else:
                             logging.info(f"O saldo é inferior a $ {minimum_balance} o saldo atual é:{balance}.\n")
